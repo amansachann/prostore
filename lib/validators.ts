001400ef77f0854ab38af5ab17bcb14865ef11e8
Aman Sachan <codewithaman78@gmail.com>
2025-01-21 04:28:19 +0530

    config: serverless environment config

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
 import { formatNumberWithDecimal } from "./utils";
 
-const currency = z
-  .string()
+const currency = z.string()
   .refine(
     (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
     "Price must have atleast 2 decimal places"
@@ -19,5 +18,5 @@ export const insertProductSchema = z.object({
   images: z.array(z.string()).min(1, "Product must have at least one image"),
   isFeatured: z.boolean(),
   banner: z.string().nullable(),
-  price: currency ,
+  price: currency,
 });
