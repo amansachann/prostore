b8bb69e21bbfd99ee966837bf4527bfd79ffe5b2
Aman Sachan <codewithaman78@gmail.com>
2025-01-21 00:51:16 +0530

    feat: theme-toggler

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
 import Link from "next/link";
 import Image from "next/image";
+import ModeToggle from "./mode-toggle";
 
 const Header = () => {
   return (
@@ -25,12 +26,13 @@ const Header = () => {
             </Link>
           </div>
           <div className="space-x-2">
+            <ModeToggle />
             <Button asChild variant={"ghost"}>
               <Link href={"/cart"}>
                 <ShoppingCart /> Cart
               </Link>
             </Button>
-            <Button asChild variant={"ghost"}>
+            <Button asChild>
               <Link href={"/sign-in"}>
                 <UserIcon /> Sign In
               </Link>
