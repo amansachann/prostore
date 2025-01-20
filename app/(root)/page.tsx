089bbf3cc81a6c3b6073de9dc5bcf60a44100f7f
Aman Sachan <codewithaman78@gmail.com>
2025-01-21 01:52:46 +0530

    add: sample data & product list component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,12 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
+
 // const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export default function Home() {
-
-  return <>Prostore</>;
+  return (
+    <>
+      <ProductList data={sampleData.products} title="Newest Arrivals" limit={4} />
+    </>
+  );
 }
