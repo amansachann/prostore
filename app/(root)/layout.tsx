90e37886d557053751a71221efe7078be998f128
Aman Sachan <codewithaman78@gmail.com>
2025-01-21 00:35:55 +0530

    add: footer component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { APP_NAME } from "@/lib/constants";
 import Header from "@/components/shared/header";
+import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
   title: `${APP_NAME}`,
@@ -16,6 +17,7 @@ export default function RootLayout({
     <div className="flex h-screen flex-col">
       <Header />
       <main className="flex-1 wrapper">{children}</main>
+      <Footer />
     </div>
   );
 }
