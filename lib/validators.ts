cfce27f549543902c16d541a9c94038e3d9d7351
Aman Sachan <codewithaman78@gmail.com>
2025-01-27 04:05:46 +0530

    add: sign in and sign ouut server actions

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 import { formatNumberWithDecimal } from "./utils";
 
-const currency = z.string()
+const currency = z
+  .string()
   .refine(
     (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
     "Price must have atleast 2 decimal places"
@@ -19,4 +20,10 @@ export const insertProductSchema = z.object({
   isFeatured: z.boolean(),
   banner: z.string().nullable(),
   price: currency,
+});
+
+// Schema for signing users in
+export const signInFromSchema = z.object({
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().min(6, "Password must be atleast 6 characters"),
 });
